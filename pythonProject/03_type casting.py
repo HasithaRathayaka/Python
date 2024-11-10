88452cc92056abfdbef4a02d5c4f117c0e635f57
Hasitha rathnayaka <152842155+HasithaRathayaka@users.noreply.github.com>
2024-11-10 11:41:40 +0530

    Create 03_type casting.py

---
+++ pythonProject/03_type casting.py
@@ -0,0 +1,18 @@
+name ="Hasitha"
+age = 22
+gpa = 1.9
+student = True
+
+print(type(name))
+print(type(age))
+print(type(gpa))
+print(type(student))
+
+#typecasting converting a value of one data type into another data type
+age = float(age)
+print(age)
+print(type(age))
+
+gpa = int(gpa)
+print(gpa)
+print(type(gpa))
