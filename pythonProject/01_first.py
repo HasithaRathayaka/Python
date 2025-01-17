fe534bc883ffc88467a6bd73ac776bdd9927142b
Hasitha rathnayaka <152842155+HasithaRathayaka@users.noreply.github.com>
2025-01-17 10:36:56 +0530

    python 01

---
+++ pythonProject/01_first.py
@@ -0,0 +1,11 @@
+patient_name = 'John Smith'
+age = 20
+patient_type = 'new'
+
+print(patient_name)
+print(age)
+print(patient_type)
+
+#Reserving Inputs
+name = input("What is your Name? ")
+print("Hello "+name)
