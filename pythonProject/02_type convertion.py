276fb92869c09845c16ed44a01f24a5685c85ece
Hasitha rathnayaka <152842155+HasithaRathayaka@users.noreply.github.com>
2025-01-17 13:02:57 +0530

    Create 02_type convertion.py

---
+++ pythonProject/02_type convertion.py
@@ -0,0 +1,21 @@
+"""
+birth_year = input("Enter Your Birth year: ")
+age = 2025 - int(birth_year)
+print(age)
+"""
+
+#Add Two Numbers
+first_number = float(input("Enter First Number "))
+second_number = float(input("Enter Second Number "))
+Total =(first_number) + (second_number)
+print("Total: " +str(Total))
+
+number1 = 20
+number2 = 30
+total = int(number1)*int(number2)
+print(total)
+
+number1 = 40
+number2 = 30
+total = int(number1)+int(number2)
+print(total)
